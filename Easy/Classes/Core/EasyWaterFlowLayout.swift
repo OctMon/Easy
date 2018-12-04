491af3077bb8791bd5ff842634ef995d9b260c61
OctMon <octmon@qq.com>
2018-12-04 14:09:35 +0800

    refactor

--- Easy/Classes/Core/EasyWaterFlowLayout.swift
+++ Easy/Classes/Core/EasyWaterFlowLayout.swift
@@ -13,14 +13,6 @@ public extension Easy {
 
 public class EasyWaterFlowLayout: UICollectionViewFlowLayout {
     
-    /// 瀑布流样式
-    ///
-    /// - equalWidth: 等宽不等高(实现宽无效、高有效)
-    /// - equalHeight: 等高不等宽(实现宽、高都有效)
-    public enum FlowStyle {
-        case equalWidth, equalHeight
-    }
-    
     public weak var delegate: UICollectionViewDelegateFlowLayout?
     
     public var flowStyle: FlowStyle = .equalWidth
@@ -50,6 +42,18 @@ public class EasyWaterFlowLayout: UICollectionViewFlowLayout {
     
 }
 
+public extension EasyWaterFlowLayout {
+    
+    /// 瀑布流样式
+    ///
+    /// - equalWidth: 等宽不等高(实现宽无效、高有效)
+    /// - equalHeight: 等高不等宽(实现宽、高都有效)
+    enum FlowStyle {
+        case equalWidth, equalHeight
+    }
+    
+}
+
 extension EasyWaterFlowLayout {
     
     public override func prepare() {
@@ -58,16 +62,18 @@ extension EasyWaterFlowLayout {
         guard let collectionView = collectionView else { return }
         guard let delegate = delegate else { return }
         
-        if scrollDirection == .vertical {
-            if flowStyle == .equalWidth {
-                // 设置宽无效、高有效 列数有效、行数无效
+        switch scrollDirection {
+        case .vertical:
+            switch flowStyle {
+            case .equalWidth:
+                // 设置宽无效、高有效
                 maxContentHeight = 0
                 heights.removeAll()
                 for _ in 0..<flowCount {
                     heights.append(sectionInset.top)
                 }
-            } else {
-                // 设置宽高都有效 列数和行数无效
+            case .equalHeight:
+                // 设置宽高都有效
                 maxContentHeight = 0
                 heights.removeAll()
                 heights.append(sectionInset.top)
@@ -76,8 +82,11 @@ extension EasyWaterFlowLayout {
                 widths.removeAll()
                 widths.append(sectionInset.left)
             }
-        } else {
-            if flowStyle == .equalHeight {
+        case .horizontal:
+            switch flowStyle {
+            case .equalWidth:
+                break
+            case .equalHeight:
                 // 设置宽有效、高无效
                 maxContentWidth = 0
                 widths.removeAll()
@@ -110,7 +119,7 @@ extension EasyWaterFlowLayout {
     
     public override func layoutAttributesForSupplementaryView(ofKind elementKind: String, at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
         let layoutAttributes = UICollectionViewLayoutAttributes(forSupplementaryViewOfKind: elementKind, with: indexPath)
-        layoutAttributes.frame = getHeader(forSupplementaryViewOfKind: elementKind, with: indexPath)
+        layoutAttributes.frame = getSupplementaryView(ofKind: elementKind, with: indexPath)
         return layoutAttributes
     }
     
@@ -132,52 +141,51 @@ extension EasyWaterFlowLayout {
 
 private extension EasyWaterFlowLayout {
     
-    func getHeader(forSupplementaryViewOfKind elementKind: String, with indexPath: IndexPath) -> CGRect {
+    func getSupplementaryView(ofKind elementKind: String, with indexPath: IndexPath) -> CGRect {
         guard let collectionView = collectionView else { return .zero }
         guard let delegate = delegate else { return .zero }
+        guard scrollDirection == .vertical else { return .zero }
+        
         if elementKind == UICollectionView.elementKindSectionHeader {
-            if scrollDirection == .vertical {
-                var height: CGFloat = 0
-                if delegate.responds(to: #selector(delegate.collectionView(_:layout:referenceSizeForHeaderInSection:))) {
-                    height = delegate.collectionView!(collectionView, layout: self, referenceSizeForHeaderInSection: indexPath.section).height
-                }
-                var y = maxContentHeight == 0 ? sectionInset.top : maxContentHeight
-                if !delegate.responds(to: #selector(delegate.collectionView(_:layout:referenceSizeForFooterInSection:))) || delegate.collectionView!(collectionView, layout: self, referenceSizeForFooterInSection: indexPath.section).height == 0 {
-                    y = maxContentHeight == 0 ? sectionInset.top : maxContentHeight + CGFloat(minimumLineSpacing)
-                }
-                maxContentHeight = y + height
-                if flowStyle == .equalWidth {
-                    heights.removeAll()
-                    for _ in 0..<flowCount {
-                        heights.append(maxContentHeight)
-                    }
-                } else {
-                    widths[0] = collectionView.width
-                    heights[0] = maxContentHeight
-                }
-                return CGRect(x: 0, y: y, width: collectionView.width, height: height)
+            var height: CGFloat = 0
+            if delegate.responds(to: #selector(delegate.collectionView(_:layout:referenceSizeForHeaderInSection:))) {
+                height = delegate.collectionView!(collectionView, layout: self, referenceSizeForHeaderInSection: indexPath.section).height
             }
+            var y = maxContentHeight == 0 ? sectionInset.top : maxContentHeight
+            if !delegate.responds(to: #selector(delegate.collectionView(_:layout:referenceSizeForFooterInSection:))) || delegate.collectionView!(collectionView, layout: self, referenceSizeForFooterInSection: indexPath.section).height == 0 {
+                y = maxContentHeight == 0 ? sectionInset.top : maxContentHeight + CGFloat(minimumLineSpacing)
+            }
+            maxContentHeight = y + height
+            switch flowStyle {
+            case .equalWidth:
+                heights.removeAll()
+                for _ in 0..<flowCount {
+                    heights.append(maxContentHeight)
+                }
+            case .equalHeight:
+                widths[0] = collectionView.width
+                heights[0] = maxContentHeight
+            }
+            return CGRect(x: 0, y: y, width: collectionView.width, height: height)
         } else {
-            if scrollDirection == .vertical {
-                var height: CGFloat = 0
-                if delegate.responds(to: #selector(delegate.collectionView(_:layout:referenceSizeForFooterInSection:))) {
-                    height = delegate.collectionView!(collectionView, layout: self, referenceSizeForFooterInSection: indexPath.section).height
-                }
-                let y = height == 0 ? maxContentHeight : maxContentHeight + CGFloat(minimumLineSpacing)
-                maxContentHeight = y + height
-                if flowStyle == .equalWidth {
-                    heights.removeAll()
-                    for _ in 0..<flowCount {
-                        heights.append(maxContentHeight)
-                    }
-                } else {
-                    widths[0] = collectionView.width
-                    heights[0] = maxContentHeight
-                }
-                return CGRect(x: 0, y: y, width: collectionView.width, height: height)
+            var height: CGFloat = 0
+            if delegate.responds(to: #selector(delegate.collectionView(_:layout:referenceSizeForFooterInSection:))) {
+                height = delegate.collectionView!(collectionView, layout: self, referenceSizeForFooterInSection: indexPath.section).height
             }
+            let y = height == 0 ? maxContentHeight : maxContentHeight + CGFloat(minimumLineSpacing)
+            maxContentHeight = y + height
+            switch flowStyle {
+            case .equalWidth:
+                heights.removeAll()
+                for _ in 0..<flowCount {
+                    heights.append(maxContentHeight)
+                }
+            case .equalHeight:
+                widths[0] = collectionView.width
+                heights[0] = maxContentHeight
+            }
+            return CGRect(x: 0, y: y, width: collectionView.width, height: height)
         }
-        return .zero
     }
     
     func getItem(with indexPath: IndexPath) -> CGRect {
@@ -187,8 +195,10 @@ private extension EasyWaterFlowLayout {
         if delegate.responds(to: #selector(delegate.collectionView(_:layout:sizeForItemAt:))) {
             size = delegate.collectionView!(collectionView, layout: self, sizeForItemAt: indexPath)
         }
-        if scrollDirection == .vertical {
-            if flowStyle == .equalWidth {
+        switch scrollDirection {
+        case .vertical:
+            switch flowStyle {
+            case .equalWidth:
                 let width = (collectionView.width - sectionInset.left - sectionInset.right - CGFloat(flowCount - 1) * minimumInteritemSpacing) / CGFloat(flowCount)
                 let min = heights.enumerated().min { a, b in a.element < b.element } ?? (0, heights[0])
                 let x = sectionInset.left + CGFloat(min.offset) * (width + minimumInteritemSpacing)
@@ -203,7 +213,7 @@ private extension EasyWaterFlowLayout {
                     maxContentHeight = height
                 }
                 return frame
-            } else {
+            case .equalHeight:
                 var headerSize: CGSize = .zero
                 if delegate.responds(to: #selector(delegate.collectionView(_:layout:referenceSizeForHeaderInSection:))) {
                     headerSize = delegate.collectionView!(collectionView, layout: self, referenceSizeForHeaderInSection: indexPath.section)
@@ -233,8 +243,11 @@ private extension EasyWaterFlowLayout {
                 maxContentHeight = heights.first ?? 0
                 return CGRect(x: x, y: y, width: size.width, height: size.height)
             }
-        } else {
-            if flowStyle == .equalHeight {
+        case .horizontal:
+            switch flowStyle {
+            case .equalWidth:
+                return .zero
+            case .equalHeight:
                 let height = (collectionView.height - sectionInset.top - sectionInset.bottom - CGFloat(flowCount - 1) * minimumLineSpacing) / CGFloat(flowCount)
                 let min = widths.enumerated().min { a, b in a.element < b.element } ?? (0, widths[0])
                 var x = min.element
@@ -251,7 +264,6 @@ private extension EasyWaterFlowLayout {
                 return frame
             }
         }
-        return .zero
     }
     
 }
