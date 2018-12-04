67e83fdf7f2b8faccb1756ab6832d66940ea3a41
OctMon <octmon@qq.com>
2018-12-04 17:29:11 +0800

    remove EasyRSA

--- Easy/Classes/RSA/EasyRSA.swift
+++ Easy/Classes/RSA/EasyRSA.swift
@@ -8,51 +8,33 @@
 import Foundation
 import SwiftyRSA
 
-public extension Easy {
-    typealias RSA = EasyRSA
-}
-
-public struct EasyRSA {
-    var publicKey = ""
-    var privateKey = ""
-    
-    static var shared = EasyRSA()
-    
-    private init() { }
-    
-    public static func set(publicKey: String, privateKey: String) {
-        shared.publicKey = publicKey
-        shared.privateKey = privateKey
-    }
-}
-
 public extension String {
     
-    func encryptRSA(using encoding: String.Encoding = .utf8, padding: Padding = .PKCS1) -> EncryptedMessage? {
-        guard let publicKey = try? PublicKey(pemEncoded: EasyRSA.shared.publicKey) else { return nil }
+    func encryptRSA(using encoding: String.Encoding = .utf8, padding: Padding = .PKCS1, publicKey: String) -> EncryptedMessage? {
+        guard let publicKey = try? PublicKey(pemEncoded: publicKey) else { return nil }
         guard let clear = try? ClearMessage(string: self, using: encoding) else { return nil }
         guard let encrypted = try? clear.encrypted(with: publicKey, padding: padding) else { return nil }
         return encrypted
     }
     
-    func encryptRSA_toString(using encoding: String.Encoding = .utf8, padding: Padding = .PKCS1) -> String? {
-        return encryptRSA(using: encoding, padding: padding)?.base64String
+    func encryptRSA_toString(using encoding: String.Encoding = .utf8, padding: Padding = .PKCS1, publicKey: String) -> String? {
+        return encryptRSA(using: encoding, padding: padding, publicKey: publicKey)?.base64String
     }
     
-    func decryptRSA(padding: Padding = .PKCS1) -> ClearMessage? {
-        guard let privateKey = try? PrivateKey(pemEncoded: EasyRSA.shared.privateKey) else { return nil }
+    func decryptRSA(padding: Padding = .PKCS1, privateKey: String) -> ClearMessage? {
+        guard let privateKey = try? PrivateKey(pemEncoded: privateKey) else { return nil }
         guard let encrypted = try? EncryptedMessage(base64Encoded: self) else { return nil }
         guard let clear = try? encrypted.decrypted(with: privateKey, padding: padding) else { return nil }
         return clear
     }
     
-    func decryptRSA_toString(using encoding: String.Encoding = .utf8, padding: Padding = .PKCS1) -> String {
-        guard let string = try? decryptRSA(padding: padding)?.string(encoding: encoding) else { return "" }
+    func decryptRSA_toString(using encoding: String.Encoding = .utf8, padding: Padding = .PKCS1, privateKey: String) -> String {
+        guard let string = try? decryptRSA(padding: padding, privateKey: privateKey)?.string(encoding: encoding) else { return "" }
         return string ?? ""
     }
     
-    func decryptRSA_toBase64(padding: Padding = .PKCS1) -> String {
-        return decryptRSA(padding: padding)?.base64String ?? ""
+    func decryptRSA_toBase64(padding: Padding = .PKCS1, privateKey: String) -> String {
+        return decryptRSA(padding: padding, privateKey: privateKey)?.base64String ?? ""
     }
     
 }
