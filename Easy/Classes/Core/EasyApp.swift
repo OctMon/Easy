772b4bbf3ebfe220a38d6bdc212c267b324a498a
OctMon <octmon@qq.com>
2018-12-05 13:57:14 +0800

    add timestamp

--- Easy/Classes/Core/EasyApp.swift
+++ Easy/Classes/Core/EasyApp.swift
@@ -57,6 +57,8 @@ public extension EasyApp {
     static let isCameraAvailableFront = UIImagePickerController.isCameraDeviceAvailable(.front)
     static let isCameraAvailableRear = UIImagePickerController.isCameraDeviceAvailable(.rear)
     
+    static var timestampMillis: Int { return ((Date().timeIntervalSince1970) * 1000).toInt }
+    static var timestamp: Int { return Date().timeIntervalSince1970.toInt }
     static var randomUUID: String { return UUID().uuidString }
     static let getKeychainUUID: String = EasyKeychain.getUUID(service: "EasyKeychain") ?? ""
     
